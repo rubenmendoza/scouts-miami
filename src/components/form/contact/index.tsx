4e7c8c6955f60c61d5b17a15c90a3d3c09f54ad3
rubenmendoza <info@rubenmendoza.com>
2023-02-01 13:19:51 +0100

    Feature: Refactored  Contact Form

--- src/components/form/contact/index.tsx
+++ src/components/form/contact/index.tsx
@@ -1,17 +1,61 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import InputField from "../input";
 import Form from "../";
 
-const ContactForm = () => {
-  const onSubmit = (data: FormData) => {
-    const emaildata = Object.fromEntries(data.entries());
-    const emailContent = `
-    Message received from <strong>${emaildata.name}</strong>. 
-    Their email address is <strong>${emaildata.email}</strong>. <br />
-    They'd like to know about...
-    ${emaildata.message}
+interface IContactFormData {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+}
+
+interface IOnSubmit<T> {
+  (data: T): void;
+}
+
+const ContactForm: React.FC = () => {
+  const [formData, setFormData] = useState<IContactFormData>({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const onChange = (e: FormEvent) => {
+    const { name, value } = e.target as HTMLInputElement;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const onSubmit: IOnSubmit<IContactFormData> = async (
+    data: IContactFormData
+  ) => {
+    const apiURL = "https://scouttroop247.com/api";
+    const message = `
+    Message received from <strong>${data.name}</strong>.<br /> 
+    Email address is <strong>${data.email}</strong>. <br />
+    Message:<br />
+    ${data.message}
     `;
-    console.log(emaildata);
+    await fetch(apiURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: JSON.stringify({
+        email: data.email,
+        subject: data.subject,
+        message: message,
+      }),
+    }).then(async (res) => {
+      const resData = await res;
+      console.log(resData);
+      if (resData.status === 200) {
+        alert("Message Sent!");
+      } else {
+        alert("Message failed to send");
+      }
+    });
+    console.log(message);
   };
   return (
     <Form onSubmit={onSubmit}>
@@ -24,6 +68,7 @@ const ContactForm = () => {
           placeholder="John Doe"
           label="Your Full Name"
           required
+          onChange={onChange}
         />
         <InputField
           id="email"
@@ -32,6 +77,7 @@ const ContactForm = () => {
           placeholder="johndoe@gmail.com"
           label="Your Email"
           required
+          onChange={onChange}
         />
         <InputField
           id="subject"
@@ -40,10 +86,16 @@ const ContactForm = () => {
           placeholder="Comment"
           label="Subject"
           required
+          onChange={onChange}
         />
         <span>
           <label htmlFor="message">Message</label>
-          <textarea name="message" id="message"></textarea>
+          <textarea
+            name="message"
+            id="message"
+            onChange={onChange}
+            value={formData.message}
+          ></textarea>
         </span>
         <button>Send</button>
       </fieldset>
